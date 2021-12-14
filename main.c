0ae82483bbd8f96a11b815285aee8350a72a114d
Marien Moutima-Backenga <marien.moutima-backenga@edu.devinci.fr>
2021-12-14 17:36:45 +0100

    guessing game V2

--- main.c
+++ main.c
@@ -2,38 +2,67 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-int main(){
-	srand(time(NULL));
-	int nombre=rand()%100;
-	int devine=0;
-	printf("I have in minde a number in between 1 and 100, can you find it?\n");
-	while(devine!=nombre){
-		scanf("%d",&devine);
-		if(devine<1){
-			printf("Invalid input");
-			return 1;
+
+int main(int argc, char *argv[]){
+	if(argc==1){
+		srand(time(NULL));
+		int devine = rand() % 100+ 1;
+		int guess = 0;
+		printf("I have in mind a number in between 1 and 100, can you find it?\n");
+		while (guess != devine){
+			scanf("%d", &guess);
+			if(guess >= 1 && guess <= 100){
+				if(guess > devine){
+					printf("The number to guess is lower\n");
+				}
+				else if (devine > guess){
+				       printf("The number to guess is higher\n");
+				}
+			}
+			else{
+				printf("Invalid intput\n");
+				return 1;
+
+			}
 		}
-		if (devine>100){
-			printf("Invalid input");
-			return 1;
-		}
-		else if(nombre>devine){
-			printf("The number to guess is higher\n");
-		}
-		else if(nombre<devine){
-			printf("The number to guess is lower\n");
-		}
-		else if(devine >='a' && devine <='z'){
-			printf("Invalid input");
-			return 1;
-		}
-		else if(devine >='A' && devine <='Z'){
-			printf("Invalid input");
-			return 1;
-		}
-		else{
-			printf("You just found the number, it was indeen %d", devine);
-		}
+		printf("You found the number, it was indeed %d\n",guess);
+		return 0;
 	}
-	return 0;
+
+		else if ( atoi(argv[1]) != 0){
+			srand(time(NULL));
+			int devine = rand() % atoi(argv[1]) + 1;
+			int guess =0;
+			printf("I have in mind a number in between 1 and %d, can you find it?\n",atoi(argv[1]));
+			while (guess != devine){
+				scanf("%d", &guess);
+				if(guess >= 1 && guess <= atoi(argv[1])){
+                                if(guess > devine){
+                                        printf("The number to guess is lower\n");
+                                }
+                                else if (devine > guess){
+                                       printf("The number to guess is higher\n");
+                                }
+                        }
+                        else{
+                                printf("Invalid intput\n");
+                                return 1;
+
+                        
+			}
+			}
+                
+                printf("You found the number, it was indeed %d\n",guess);
+                return 0;
+		}
+			else{
+				printf("Invalid input\n");
+				return 1;
+			
+			}
 }
+
+
+
+
+
